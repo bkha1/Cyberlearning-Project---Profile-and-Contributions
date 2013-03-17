47b7867d21bceea2492b5fa958bb77e3d735c2f7
Elissa <eethomas@asu.edu>
2013-03-17 15:27:45 -0700

    Add dragging capability to toolbar widgets

--- src/cyber/learning/project/client/CyberLearning.java
+++ src/cyber/learning/project/client/CyberLearning.java
@@ -1,9 +1,20 @@
 package cyber.learning.project.client;
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
+import gwtquery.plugins.draggable.client.gwt.DraggableWidget;
+
+import com.google.gwt.core.client.EntryPoint; 
+import com.google.gwt.dom.client.Style.Cursor;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.dom.client.MouseUpEvent;
+import com.google.gwt.event.dom.client.MouseUpHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -11,46 +22,35 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.client.ui.ValueBoxBase.TextAlignment;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class CyberLearning implements EntryPoint {
-	
-	/**
-	 * Declare variables
-	 */
-	boolean toolbarVisible = false;
-	
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-
+	private boolean toolbarVisible = true;
+	private boolean resizeEnabled = false;
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
 		final Label errorLabel = new Label();
-
+	
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
 		RootPanel rootPanel = RootPanel.get("bookContainer");
 		RootPanel.get("errorLabelContainer").add(errorLabel);
+
 		
 		VerticalPanel flowPanel = new VerticalPanel();
 		rootPanel.add(flowPanel, 100, 100);
@@ -62,30 +62,72 @@ public class CyberLearning implements EntryPoint {
 		MenuItem mntmSearchtext = new MenuItem("Table of Contents", false, (Command) null);
 		menuBar.addItem(mntmSearchtext);
 		
-		HorizontalPanel horizontalPanel = new HorizontalPanel();
-		flowPanel.add(horizontalPanel);
-		horizontalPanel.setSize("100%", "500px");
+		HorizontalPanel contentHorizontalPanel = new HorizontalPanel();
+		flowPanel.add(contentHorizontalPanel);
+		contentHorizontalPanel.setSize("100%", "500px");
 		
 		final VerticalPanel toolbarPanel = new VerticalPanel();
-		horizontalPanel.add(toolbarPanel);
+		contentHorizontalPanel.add(toolbarPanel);
 		toolbarPanel.setSize("80px", "144px");
 		
-		Button btnNewButton = new Button("New button");
-		btnNewButton.setText("Button");
-		toolbarPanel.add(btnNewButton);
-		btnNewButton.setWidth("101px");
-		
+		//create "Text" item
 		TextArea txtText = new TextArea();
-		txtText.setText("Text");		
-		toolbarPanel.add(txtText);
-		txtText.setWidth("100%");
+		txtText.setAlignment(TextAlignment.CENTER);
+		txtText.setText("Text");
+		//configure as draggable and add to toolbar
+		final Widget draggableText = createDraggableText(txtText);
+		toolbarPanel.add(draggableText);
+		draggableText.setSize("90%", "75px");
+		//use "CTRL" key to alternate between drag and resize mode
+		txtText.addMouseDownHandler(new MouseDownHandler()
+		{
+
+			@Override
+			public void onMouseDown(MouseDownEvent event) {
+				//if "CTRL" key pressed, user resizing widget
+				if(event.isControlKeyDown())
+				{
+					((DraggableWidget<TextArea>) draggableText).setDisabledDrag(true);
+				}
+			}
+			
+		});
+		txtText.addMouseUpHandler(new MouseUpHandler()
+		{
+			@Override
+			public void onMouseUp(MouseUpEvent event) {
+				((DraggableWidget<TextArea>) draggableText).setDisabledDrag(false);
+			}
+			
+		});
 		
+		//create "Image" item
 		Image image = new Image("cyberlearning/gwt/clean/images/image_icon.PNG");
-		toolbarPanel.add(image);
-		image.setSize("92px", "80px");
+		//configure as draggable and add to toolbar
+		Widget draggableImage = createDraggableImage(image);
+		toolbarPanel.add(draggableImage);
+		draggableImage.setSize("100%", "90px");
+		toolbarPanel.setCellHorizontalAlignment(draggableImage, HasHorizontalAlignment.ALIGN_CENTER);
+		
+		//create "Video" item
+		Image video = new Image("cyberlearning/gwt/clean/images/video_icon.png");
+		//configure as draggable and add to toolbar
+		Widget draggableVideo = createDraggableImage(video);
+		toolbarPanel.add(draggableVideo);
+		toolbarPanel.setCellHorizontalAlignment(draggableVideo, HasHorizontalAlignment.ALIGN_CENTER);
+		draggableVideo.setSize("90%", "100px");
+		
+		//create "Table" item
+		//CellTable<Object> cellTable = new CellTable<Object>();
+		//Column<Object, ?> col = null;
+		//cellTable.addColumn(col);
+		//configure as draggable and add to toolbar		
+		//Widget draggableTable = createDraggableTable(cellTable);
+		//toolbarPanel.add(draggableTable);
+		//draggableTable.setSize("90%", "141px");
 		
 		final FlexTable contentPanel = new FlexTable();
-		horizontalPanel.add(contentPanel);
+		contentHorizontalPanel.add(contentPanel);
 		contentPanel.setSize("885px", "100%");
 		
 		TextArea someText = new TextArea();
@@ -93,8 +135,12 @@ public class CyberLearning implements EntryPoint {
 		contentPanel.setWidget(0, 0, someText);
 		someText.setSize("100%", "500px");
 		
+		HorizontalPanel editHorizontalPanel_1 = new HorizontalPanel();
+		flowPanel.add(editHorizontalPanel_1);
+		editHorizontalPanel_1.setWidth("100%");
+		
 		Button viewToolbarBtn = new Button("New button");
-		flowPanel.add(viewToolbarBtn);
+		editHorizontalPanel_1.add(viewToolbarBtn);
 		viewToolbarBtn.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				toolbarVisible = !toolbarVisible;
@@ -113,5 +159,39 @@ public class CyberLearning implements EntryPoint {
 			}
 		});
 		viewToolbarBtn.setText("View Toolbar");
+		
+		TextArea textArea = new TextArea();
+		textArea.setAlignment(TextAlignment.LEFT);
+		editHorizontalPanel_1.add(textArea);
+		textArea.setWidth("100%");
+	}
+	
+	Widget createDraggableText(TextArea txtArea)
+	{
+		//configure as draggable
+		DraggableWidget<TextArea> draggableText = new DraggableWidget<TextArea>(txtArea);
+		draggableText.setDraggingCursor(Cursor.MOVE);
+		
+		return draggableText;
+	}
+	
+	Widget createDraggableImage(Image img)
+	{
+		//configure as draggable
+		DraggableWidget<Image> draggableImage = new DraggableWidget<Image>(img);
+		draggableImage.setDraggingCursor(Cursor.MOVE);
+		draggableImage.setDisabledDrag(true);
+		
+		return draggableImage;
+	}
+	
+	Widget createDraggableTable(CellTable<Object> table)
+	{
+		//configure as draggable
+		DraggableWidget<CellTable<Object>> draggableTable = new DraggableWidget<CellTable<Object>>(table);
+		draggableTable.setDraggingCursor(Cursor.MOVE);
+		draggableTable.setDisabledDrag(true);
+		
+		return draggableTable;
 	}
 }
